6bcac7a8c2d3d19d9f6bb1103219924cab664146
Hbl41 <brady4sears@gmail.com>
2026-04-23 21:32:15 +0000

    Tighten header spacing more

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Network-first for HTML (so deploys update), cache-first for static assets.
 // Skips anything that isn't same-origin.
 
-const CACHE_NAME = 'iosandros-v6';
+const CACHE_NAME = 'iosandros-v7';
 const PRECACHE = [
   './',
   './index.html',
