f5827ff4c2029e7a0c8a1c945e281da3d74bde1a
Hbl41 <brady4sears@gmail.com>
2026-04-24 00:56:19 +0000

    iPad landscape: keep trackers/search/actions on one row; shorten header

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Network-first for HTML (so deploys update), cache-first for static assets.
 // Skips anything that isn't same-origin.
 
-const CACHE_NAME = 'iosandros-v9';
+const CACHE_NAME = 'iosandros-v10';
 const PRECACHE = [
   './',
   './index.html',
